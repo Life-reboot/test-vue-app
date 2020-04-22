212d0294b2103a8c911d4a57e7494a3f13ae0ee3
Peter Jang <peter.x.jang@gmail.com>
2020-04-21 21:03:48 -0500

    Add example 3rd party Vue components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h2>Hello</h2>
+    <autocomplete
+      :source="[
+        { id: 1, name: 'abc' },
+        { id: 2, name: 'def' },
+      ]"
+    ></autocomplete>
+
+    <autocomplete
+      source="https://api.github.com/search/repositories?q="
+      results-property="items"
+      results-display="full_name"
+    ></autocomplete>
+
+    <pie-chart
+      :data="[
+        ['Blueberry', 44],
+        ['Strawberry', 23],
+        ['Apples', 23],
+      ]"
+    ></pie-chart>
+    <multiselect v-model="value" :options="options" :multiple="true"></multiselect>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Multiselect from "vue-multiselect";
+import Vue from "vue";
+import Chartkick from "vue-chartkick";
+import Chart from "chart.js";
+import Autocomplete from "vuejs-auto-complete";
 
+Vue.use(Chartkick.use(Chart));
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  // OR register locally
+  components: { Multiselect, Autocomplete },
+  data() {
+    return {
+      value: null,
+      options: ["list", "of", "options"],
+    };
+  },
+};
 </script>
+
+<!-- New step!
+     Add Multiselect CSS. Can be added as a static asset or inside a component. -->
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
+
+<style></style>
